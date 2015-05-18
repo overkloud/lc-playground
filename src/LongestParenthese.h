642d05226ff30d79dc69bf1722df6532d6b6f68f
Jeff Jiang <jeffrey.yt.jiang@gmail.com>
2015-05-18 02:33:39 +0100

    add longest parenthese

--- src/LongestParenthese.h
+++ src/LongestParenthese.h
@@ -12,85 +12,83 @@ class LongestParenthese : public solution
 {
 
 public:
-	static int longestValidParentheses(string s) {
+    static int longestValidParentheses(string s) {
 
-		stack <char> t;
-		int maxLength = 0;
-		int start = -1;
-		int end = -1;
-		for(int i = 0; i < s.length(); i++)
-		{
-			char c  = s.at(i);
-			if(c == '(')
-			{
-				t.push(c);
-				if(start == -1) start = i;
-			}
-			else
-			{
-				if(t.size() > 0)
-				{
-					char cc = t.top();
-					t.pop();
-					//end = i;
-					if(i - start > maxLength && t.size() == 0)
-					{
-						end = i;
-						maxLength = end - start;
-					}
-				}
-				else
-				{
-					stack<char> tt;
-					t.swap(tt);
-					start = -1;
-					end = -1;
-				}
-			}
-
-		for(int i = s.length() - 1; i >= 0 ; i--)
-		{
-			char c  = s.at(i);
-			if(c == ')')
-			{
-				t.push(c);
-				if(start == -1) start = i;
-			}
-			else
-			{
-				if(t.size() > 0)
-				{
-					char cc = t.top();
-					t.pop();
-					//end = i;
-					if(i - start > maxLength && t.size() == 0)
-					{
-						end = i;
-						maxLength = end - start;
-					}
-				}
-				else
-				{
-					stack<char> tt;
-					t.swap(tt);
-					start = -1;
-					end = -1;
-				}
-			}
-
-		}
+        stack <char> t;
+        int maxLength = 0;
+        int start = -1;
+        int end = -1;
+        for (int i = 0; i < s.length(); i++)
+        {
+            char c = s.at(i);
+            if (c == '(')
+            {
+                t.push(c);
+                if (start == -1) start = i;
+            }
+            else
+            {
+                if (t.size() > 0)
+                {
+                    char cc = t.top();
+                    t.pop();
+                    //end = i;
+                    if (i - start > maxLength)
+                    {
+                        end = i;
+                        maxLength = end - start + 1;
+                    }
+                }
+                else
+                {
+                    stack<char> tt;
+                    t.swap(tt);
+                    start = -1;
+                    end = -1;
+                }
+            }
+        }
+        //for (int i = s.length() - 1; i >= 0; i--)
+        //{
+        //    char c = s.at(i);
+        //    if (c == ')')
+        //    {
+        //        t.push(c);
+        //        if (start == -1) start = i;
+        //    }
+        //    else
+        //    {
+        //        if (t.size() > 0)
+        //        {
+        //            char cc = t.top();
+        //            t.pop();
+        //            //end = i;
+        //            if (i - start > maxLength)
+        //            {
+        //                end = i;
+        //                maxLength = end - start;
+        //            }
+        //        }
+        //        else
+        //        {
+        //            stack<char> tt;
+        //            t.swap(tt);
+        //            start = -1;
+        //            end = -1;
+        //        }
+        //    }
+        //}
 
         return maxLength;
     }
 
-	void test()
-	{
-		assert(longestValidParentheses("(()()") == 4);
-		assert(longestValidParentheses("()") == 2);
-		assert(longestValidParentheses("(()") == 2);
-		assert(longestValidParentheses("()(()") == 2);		
-
-	}
+    void test()
+    {
+        assert(longestValidParentheses("(()()") == 4);
+        assert(longestValidParentheses("()") == 2);
+        assert(longestValidParentheses("(()") == 2);
+        assert(longestValidParentheses("()(()") == 2);
+    }
 };
 
 
