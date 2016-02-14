cda90a6eff6dd62be037bc2b1d6c61ec0bf3a058
Jeff Jiang <jeffrey.yt.jiang@gmail.com>
2016-02-14 01:57:00 +0000

    remove incorrect comments

--- src/RotateImage.h
+++ src/RotateImage.h
@@ -4,17 +4,6 @@ using namespace std;
 
 class RotateImage : public  solution {
 
-    /***
-
-Suppose a sorted array is rotated at some pivot unknown to you beforehand.
-
-(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
-
-Find the minimum element.
-
-You may assume no duplicate exists in the array.
-
-    ***/
 
 public:
     void Rotate(vector<vector<int>> & v, int s_i, int s_j, int size) 
